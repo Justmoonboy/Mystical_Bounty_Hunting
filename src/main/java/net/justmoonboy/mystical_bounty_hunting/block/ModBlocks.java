44ac1e33cec2c1e0882a348e233c6decf07cdc7a
Justmoonboy <130238647+Justmoonboy@users.noreply.github.com>
2024-11-26 14:06:55 -0600

    added some (broken) block drops

--- src/main/java/net/justmoonboy/mystical_bounty_hunting/block/ModBlocks.java
+++ src/main/java/net/justmoonboy/mystical_bounty_hunting/block/ModBlocks.java
@@ -2,9 +2,11 @@ package net.justmoonboy.mystical_bounty_hunting.block;
 
 import net.justmoonboy.mystical_bounty_hunting.MysticalBountyHunting;
 import net.justmoonboy.mystical_bounty_hunting.item.ModItems;
+import net.minecraft.util.valueproviders.UniformInt;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.DropExperienceBlock;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.neoforged.bus.api.IEventBus;
@@ -18,11 +20,11 @@ public class ModBlocks {
             DeferredRegister.createBlocks(MysticalBountyHunting.MOD_ID);
 
     public static final DeferredBlock<Block> CELLESTIUM_BLOCK = registerBlock("cellestium_block",
-            () -> new Block(BlockBehaviour.Properties.of()
-                    .strength(4f).requiresCorrectToolForDrops().sound(SoundType.AMETHYST)));
+            () -> new Block(BlockBehaviour.Properties.of().strength(4f).requiresCorrectToolForDrops().sound(SoundType.AMETHYST)));
+
     public static final DeferredBlock<Block> CELLESTIUM_ORE = registerBlock("cellestium_ore",
-            () -> new Block(BlockBehaviour.Properties.of()
-                    .strength(4f).requiresCorrectToolForDrops().sound(SoundType.STONE)));
+            () -> new DropExperienceBlock(UniformInt.of(3, 6),
+        BlockBehaviour.Properties.of().strength(4f).requiresCorrectToolForDrops().sound(SoundType.STONE)));
 
     private static <T extends Block> DeferredBlock<T> registerBlock(String name, Supplier<T> block) {
         DeferredBlock<T> toReturn = BLOCKS.register(name, block);
